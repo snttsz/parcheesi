494ca96cbe4e3ef7e4e48ec5de26d913baa5be18
GS <glendasantana2099@gmail.com>
2023-04-09 19:58:26 -0300

    Defined the structure of files --> The game logic, game structure and the board drawing are now inside the game folder / The socket connection will be implemented inside the comm folder / The gameConstructor will put these structures together and compose the game itself. <--- || The initial menu should be implemented yet, probably in a new file.

---
+++ src/comm/socket.h
@@ -0,0 +1,7 @@
+#ifndef SOCKET
+#define SOCKET
+
+
+
+
+#endif
---
+++ utils/String_utils.c
@@ -0,0 +1,16 @@
+#include "String_utils.h"
+
+void writeString(char (*matrix)[MAX_COLUMNS], char *text, int line, int column)
+{
+    int i, j, size;
+
+    size = strlen(text);
+
+    for (i = column, j = 0; i < column + size; i++, j++)
+    {
+        matrix[line][i] = text[j];
+    }
+
+    return;
+}
+
